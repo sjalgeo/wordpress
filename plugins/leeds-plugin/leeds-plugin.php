e8fd496700df20b09eabe4166fa410ca03a2ee36
sjalgeo <stephen.algeo@gmail.com>
2014-10-28 10:51:06 +0000

    Updates to Leeds plugin

--- plugins/leeds-plugin/leeds-plugin.php
+++ plugins/leeds-plugin/leeds-plugin.php
@@ -16,11 +16,7 @@ function algeo_one_match_redirect() {
     if (is_search()) {
         global $wp_query;
 
-//        # If only a single result redirect to that post.
-//        if ($wp_query->post_count == 1) {
-//            wp_redirect( get_permalink( $wp_query->posts['0']->ID ) );
-//        }
-
+//        $wp_query->
         # If there is only a single search term,
         # and that matched one of the post ids
         # redirect to that post
@@ -48,4 +44,46 @@ if (!function_exists('array_lookup')){      // version of get_from_array we're p
             }
         }
     }
+}
+
+
+function ajax_debug($item){
+
+        echo '<div style="background:ghostwhite;z-index:99999;margin-left:20px;">';
+        echo '<pre>debug:<br>';
+        if (is_array($item)){
+
+            print_r($item);
+
+        } elseif (is_object($item)){
+
+            var_dump($item);
+
+        }elseif(is_numeric($item)){
+
+            echo '[numeric: '.$item.']';
+
+        }elseif($item === ''){
+            echo '[is empty string]';
+        }elseif(is_string($item)){
+
+            echo '[string: '.$item.']';
+
+        }elseif($item === true){
+
+            echo '[item is true]';
+
+        }elseif($item === false){
+
+            echo '[item is false]';
+
+        }else {
+
+            echo '[unknown]';
+            echo $item;
+            print_r($item);
+            var_dump($item);
+
+        }
+        echo '<br>------</pre></div>';
 }
